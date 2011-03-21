7b416f8e10275352197e36489f6d7783258bc8b3
Robin <robin@robin-desktop.(none)>
2011-03-21 14:20:24 +0000

    system now automatically parses package.json files of dependant modules and tries to guess the correct directory to append to the require.paths array for later inclusion

--- kree/bootstrap.js
+++ kree/bootstrap.js
@@ -1,11 +1,41 @@
 var path = require('path');
 var fs = require('fs');
 
-var libs = path.join('.', 'kree', 'libs');
-var files = fs.readdirSync(libs);
+var include_package = module.exports.include_package = function(dir, mod) {
+  var pkg = path.join(dir, mod, 'package.json');
+  var is_pkg = path.existsSync(pkg);
+  
+  if (is_pkg) {
+    var support_dir = path.join(dir, mod, 'support');
+    
+    if (path.existsSync(support_dir)) {
+      fs.readdirSync(support_dir).forEach(function(support_mod) {
+        if (fs.statSync(path.join(support_dir, support_mod)).isDirectory()) {
+          include_package(support_dir, support_mod);
+        }
+      });
+    }
+    
+    var pkg_json = JSON.parse(fs.readFileSync(pkg));
+    
+    if (pkg_json.main) {
+      if (pkg_json.main.search('index') > -1) {
+        require.paths.unshift(path.dirname(path.join(dir, mod)));
+      } else {
+        require.paths.unshift(path.dirname(path.join(dir, mod, pkg_json.main)));
+      }
+    } else {
+      require.paths.unshift(path.dirname(path.join(dir, mod)));
+    }
+  }
+};
 
-for (var f in files) {
-  require.paths.unshift(path.join(libs, files[f], 'lib'));
-}
+var resolveroot = path.resolve('./');
+var libs = path.join(resolveroot, 'kree', 'libs');
+var lib_dir = fs.readdirSync(libs)
 
-console.log(require.paths);
+lib_dir.forEach(function(library) {
+  if (fs.statSync(path.join(libs, library)).isDirectory()) {
+    include_package(libs, library);
+  }
+});
--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-require.paths.unshift('./kree');
-require('bootstrap');
+require('./kree/bootstrap');
+
+var connect = require('connect');
+
+connect.createServer(
+    connect.logger()
+  , connect.static('./webroot')
+).listen(8083);
